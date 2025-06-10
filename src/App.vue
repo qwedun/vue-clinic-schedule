b53930d5dbaa371b63e80f4cd07dcfa768347a3d
Danil Sharifullin <qweber2019@mail.ru>
2025-06-10 12:02:02 +0300

    init commit

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    123
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style>
+
+* {
+  padding: 0;
+  margin: 0;
+  border: none;
+
+  font-family:
+    Inter,
+    -apple-system,
+    BlinkMacSystemFont,
+    'Segoe UI',
+    Roboto,
+    Oxygen,
+    Ubuntu,
+    Cantarell,
+    'Fira Sans',
+    'Droid Sans',
+    'Helvetica Neue',
+    sans-serif;
+}
+*, *::before,
+*::afer {
+  box-sizing: border-box;
+}
+
+</style>
